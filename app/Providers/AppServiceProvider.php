a414bdaa7c5a785cc800b186c8f1f1ccdbffdd39
nadim imtiaz shakil <nadim@iamsheba.xyz>
2021-09-07 00:52:57 +0600

    preventing lazy loading in local environment

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Adapters\Image\ImageUploader;
 use App\Adapters\Image\InterventionImageUploader;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,6 +26,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Model::preventLazyLoading(! $this->app->isProduction());
     }
 }
