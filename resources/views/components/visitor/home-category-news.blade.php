c74f4cadf4026e00a1ff6aca1d75ed1ebfa82c97
nadim imtiaz shakil <nadim@sheba.xyz>
2021-10-17 21:49:16 +0600

    fixed home category border

--- resources/views/components/visitor/home-category-news.blade.php
+++ resources/views/components/visitor/home-category-news.blade.php
@@ -1,6 +1,6 @@
 @props(['category'])
 <div {{ $attributes }}>
-    <a href="{{ route('visitor.category', [$category->slug]) }}" class="border-l-2 border-primary leading-3 pl-1 text-gray-700 text-2xl font-bold hover:text-gray-900 transition duration-300">{{ $category->name }}</a>
+    <a href="{{ route('visitor.category', [$category->slug]) }}" class="inline-block border-l-4 border-primary leading-5 pl-1 text-gray-700 text-2xl font-bold hover:text-gray-900 transition duration-300">{{ $category->name }}</a>
 
     <div class="mt-2 grid sm:grid-cols-2 lg:grid-cols-3 gap-4">
         @foreach($category->news as $news)
